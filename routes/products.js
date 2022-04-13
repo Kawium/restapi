f3f98ab8bdc70f19366aaf2d3d5139916d4a5c6c
Kawan <kawanse0@gmail.com>
2022-04-13 15:45:06 +0200

    check if new data is added

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,35 @@
 import express from "express";
 const router = express.Router();
 
+// Mock data
+const products = [
+  {
+    productName: "iPhone",
+    brandName: "Apple",
+    ramInfo: 8,
+  },
+  {
+    productName: "Galaxy s22",
+    brandName: "Samsung",
+    ramInfo: 9,
+  },
+  {
+    productName: "Nord 2",
+    brandName: "Oneplus",
+    ramInfo: 12,
+  },
+];
+
 router.get("/", (req, res) => {
   res.send("Products");
+});
+
+router.post("/", (req, res) => {
+  const product = req.body;
+  products.push(product);
+
+  // checks if new product is added
+  res.send("Added to database");
 });
 
 export default router;
