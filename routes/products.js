1b58387ab8fac99c6213f4ee28a84643511a2764
Kawan <kawanse0@gmail.com>
2022-04-13 16:25:11 +0200

    added uuid

--- routes/products.js
+++ routes/products.js
@@ -1,35 +1,27 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
 // Mock data
-const products = [
-  {
-    productName: "iPhone",
-    brandName: "Apple",
-    ramInfo: 8,
-  },
-  {
-    productName: "Galaxy s22",
-    brandName: "Samsung",
-    ramInfo: 9,
-  },
-  {
-    productName: "Nord 2",
-    brandName: "Oneplus",
-    ramInfo: 12,
-  },
-];
+const products = [];
 
 router.get("/", (req, res) => {
   res.send("Products");
 });
 
+// adds new product
 router.post("/", (req, res) => {
   const product = req.body;
-  products.push(product);
 
-  // checks if new product is added
-  res.send("Added to database");
+  products.push({ ...product, id: uuidv4() });
+  res.send(`Product with the name ${product.productName} added to mockdata`);
+});
+
+// adds spefic id for its product
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundProduct = products.find((product) => product.id === id);
+  res.send(foundProduct);
 });
 
 export default router;
