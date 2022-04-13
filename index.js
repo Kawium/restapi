ec5035ecec37f19863faa118af2034c5306a707e
Kawan <kawanse0@gmail.com>
2022-04-13 14:48:13 +0200

    added router get and products

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import productRouter from "./routes/products.js";
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
+app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
-  console.log("Hello");
+  res.send("Main");
 });
 
 app.listen(PORT, () => {
