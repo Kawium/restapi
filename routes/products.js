3d4d1328c9c8e2dee1609129efc7b374b9349428
Kawan <kawanse0@gmail.com>
2022-04-14 18:53:05 +0200

    added put

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
 // Mock data
-const products = [];
+let products = [];
 
 router.get("/", (req, res) => {
   res.send("Products");
@@ -14,14 +14,39 @@ router.post("/", (req, res) => {
   const product = req.body;
 
   products.push({ ...product, id: uuidv4() });
-  res.send(`Product with the name ${product.productName} added to mockdata`);
+  res.send(
+    `Product with the name ${product.productName} has been added to mockdata`
+  );
 });
 
 // adds spefic id for its product
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const foundProduct = products.find((product) => product.id === id);
+  if (!foundProduct) {
+    res.status(404);
+    res.send("There is already id for that product.");
+  }
   res.send(foundProduct);
+});
+
+// delete product with spefic id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  product = products.filter((product) => product.id !== id);
+
+  res.send(`Product with id ${id} has been deleted from mockdata`);
+});
+
+// updates current ID
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const productFound = products.find((product) => product.id === id);
+  if (!productFound) {
+    res.status(404).send("Error 404: ID cannot be find.");
+  }
+  res.send(productFound);
 });
 
 export default router;
