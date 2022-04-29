8fae3df56362495b97d655f622d8512d040e8c3d
Kawan <kawanse0@gmail.com>
2022-04-29 14:08:15 +0200

    fixed get products, post, id and delete

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ will install the files to build the project
 
 ### `npm start`
 
-will start up the server
+Will start up the server
 
 The server will run at http://localhost:8000
+
+### `.rest file`
+
+Used for debugging the server.
+
+### `Requirement that has been met`
+
+1-7 för godkänt
--- routes/products.js
+++ routes/products.js
@@ -6,17 +6,20 @@ const router = express.Router();
 let products = [];
 
 router.get("/", (req, res) => {
-  res.send("Products");
+  if (products === undefined || products.length == 0) {
+    res.send("There are no products yet");
+  } else {
+    res.send(products);
+  }
 });
 
 // adds new product
 router.post("/", (req, res) => {
-  const product = req.body;
-
-  products.push({ ...product, id: uuidv4() });
-  res.send(
-    `Product with the name ${product.productName} has been added to mockdata`
-  );
+  const name = req.body.name;
+  const brand = req.body.brand;
+  const price = req.body.price;
+  products.push({ name, brand, price, id: uuidv4() });
+  res.send(`Product with the name ${name} has been added to mockdata`);
 });
 
 // adds spefic id for its product
@@ -24,19 +27,22 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
   const foundProduct = products.find((product) => product.id === id);
   if (!foundProduct) {
-    res.status(404);
-    res.send("There is already id for that product.");
+    res.status(404).send("Specified product cannot be found");
+  } else {
+    res.send(foundProduct);
   }
-  res.send(foundProduct);
 });
 
 // delete product with spefic id
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-
-  product = products.filter((product) => product.id !== id);
-
-  res.send(`Product with id ${id} has been deleted from mockdata`);
+  const foundProduct = products.find((product) => product.id === id);
+  if (!foundProduct) {
+    res.status(404).send("Specified product cannot be found");
+  } else {
+    products = products.filter((product) => product.id !== id);
+    res.send(`Product with id ${id} has been deleted from mockdata`);
+  }
 });
 
 // updates current ID
